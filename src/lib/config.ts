2feff7176e6d2034c05c3229e6619f0c283bc0ab
rimgro <rimgro.mail@gmail.com>
2025-03-17 14:28:09 +0300

    bugfix

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,2 +1,2 @@
 export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8811';
-export const YMAPS_API = import.meta.env.YMAPS_API || 'df084fff-df2c-414f-8382-2a41590c2e2a';
+export const YMAPS_API = import.meta.env.VITE_YMAPS_API || 'df084fff-df2c-414f-8382-2a41590c2e2a';
